9782c8749cf8232fa26df40cce067540a20f9bf3
Brandom <brandon.rsantillan@gmail.com>
2024-05-10 00:09:20 -0500

    Add get all, get one, and delete

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,9 +3,12 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PaginationDto } from 'src/common/dtos/pagination.dto';
 import { Repository } from 'typeorm';
+import { validate as isUUID } from 'uuid';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
@@ -29,20 +32,57 @@ export class ProductsService {
     }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll(paginationDto: PaginationDto) {
+    try {
+      return await this.productRepository.find({
+        skip: paginationDto.offset,
+        take: paginationDto.limit,
+      });
+    } catch (error) {
+      this.handleException(error);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    try {
+      let product: Product;
+
+      if (isUUID(id)) {
+        product = await this.productRepository.findOneBy({ id });
+      } else {
+        const queryBuilder =
+          this.productRepository.createQueryBuilder('product');
+
+        product = await queryBuilder
+          .where('product.slug = :slug or LOWER(product.name) = :name', {
+            slug: id.toLowerCase(),
+            name: id.toLowerCase(),
+          })
+          .getOne();
+      }
+
+      if (!product) {
+        throw new NotFoundException(`Product ${id} not found`);
+      }
+
+      return product;
+    } catch (error) {
+      this.handleException(error);
+    }
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  async update(id: string, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    try {
+      const product = await this.findOne(id);
+      await this.productRepository.remove(product);
+      return product;
+    } catch (error) {
+      this.handleException(error);
+    }
   }
 
   private handleException(error: any) {
@@ -50,6 +90,10 @@ export class ProductsService {
 
     if (error.code === '23505') {
       throw new BadRequestException(error.detail);
+    }
+
+    if (error instanceof NotFoundException) {
+      throw new NotFoundException(error.message);
     }
 
     throw new InternalServerErrorException(error.message);
